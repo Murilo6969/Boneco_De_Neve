4c314060cd878935c80fe0839c4d31917a839e5d
Murilo6969 <murilo.jose.santos@escola.pr.gov.br>
2024-09-23 15:33:40 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+   background( "cyan" );
+}
+
+function draw() {
+fill("white")
+ellipse (200, 200, 110, 110);
+stroke("white")
+ellipse (200, 300, 170, 170);
+stroke("white")  
+ellipse (200, 110, 90, 90)
+stroke("white")
+
+fill("black")
+ellipse(185, 101, 17, 17)
+fill("black")
+ellipse (215, 101, 17, 17)
+fill("black")
+
+fill (3, 3 ,3 )
+ellipse (200, 180, 12, 12)
+ellipse (200, 220, 12, 12)
+ellipse (200, 200, 12, 12)
+  
+fill ()
+}
